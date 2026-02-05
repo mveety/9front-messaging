8e08f03af8c27121e101583258d75bfbc6138c75
Matthew Veety <mveety@gmail.com>
2026-02-05 05:35:07 +0000

    move messaging constants to msg.h

--- sys/include/msg.h
+++ sys/include/msg.h
@@ -2,6 +2,19 @@
 typedef struct Mailbox Mailbox;
 typedef struct Message Message;
 
+/* message passing */
+enum {
+	Mctlread = 0,
+	Mctlwrite = 1,
+
+	MSGENABLE = (1<<0), /* allow process to receive messages */
+//	MSGMONITOR = (1<<1), /* accept monitor messages */
+//	MSGPROCS = (1<<2), /* accept process messages */
+
+	MSGALLUSERS = (1<<3), /* allow messages from other users */
+};
+
+
 struct Message {
 	s32int tag;
 	uintptr len;
